8b3789cf63293ba3862dec02e6293dea8a5216ee
nguyenvukhang <brew4k@gmail.com>
2025-01-22 17:08:32 +0800

    Colored box print

--- src/main/java/Pascal.java
+++ src/main/java/Pascal.java
@@ -1,18 +1,33 @@
+import java.io.PrintStream;
+
 public class Pascal {
     static void println(String format, Object... args) {
-        System.out.printf(format, args);
-        System.out.println();
+        PrintStream writer = System.out;
+
+        String output = String.format(format, args);
+
+        // Nothing to do here.
+        if (output.length() == 0)
+            return;
+
+        // Unwrap safety guaranteed by the fact that output is non-empty.
+        int max_line_len =
+            output.lines().map(String::length).max(Integer::compare).get();
+
+        assert max_line_len <= 70
+            : "Keep hard-coded outputs to <= 70 chars per line.";
+
+        String rule = "─".repeat(max_line_len + 2);
+        String fmt = "│ \033[36m%-" + max_line_len + "s\033[m │\n";
+
+        writer.println('╭' + rule + '╮');
+        output.lines().forEach((line) -> writer.printf(fmt, line));
+        writer.println('─' + rule + '╯');
     }
 
     public static void main(String[] args) {
-        String logo =
-            ""
-            + "____________________________________________________________\n"
-            + "  Hello! I'm Pascal!\n"
-            + "  What can I do for you?\n"
-            + "____________________________________________________________\n"
-            + "  Bye. Hope to see you again soon!\n"
-            + "____________________________________________________________";
-        println(logo);
+        String greet = "Hello! I'm Pascal!\nWhat can I do for you?\n";
+
+        println(greet);
     }
 }
