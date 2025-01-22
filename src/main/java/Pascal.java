3575c2a27ae7f61b3922623e168103ec91b74c95
nguyenvukhang <brew4k@gmail.com>
2025-01-22 16:44:59 +0800

    Format everything

--- src/main/java/Pascal.java
+++ src/main/java/Pascal.java
@@ -1,19 +1,18 @@
 public class Pascal {
-  static void println(String format, Object... args) {
-    System.out.printf(format, args);
-    System.out.println();
-  }
+    static void println(String format, Object... args) {
+        System.out.printf(format, args);
+        System.out.println();
+    }
 
-  public static void main(String[] args) {
-    String logo =
-        ""
-        + "____________________________________________________________\n"
-        + "  Hello! I'm Pascal!\n"
-        + "  What can I do for you?\n"
-        + "____________________________________________________________\n"
-        + "  Bye. Hope to see you again soon!\n"
-        + "____________________________________________________________";
-
-    println(logo);
-  }
+    public static void main(String[] args) {
+        String logo =
+            ""
+            + "____________________________________________________________\n"
+            + "  Hello! I'm Pascal!\n"
+            + "  What can I do for you?\n"
+            + "____________________________________________________________\n"
+            + "  Bye. Hope to see you again soon!\n"
+            + "____________________________________________________________";
+        println(logo);
+    }
 }
