2b28ab91f068fe7393f020f186a988bc8bc047f5
nguyenvukhang <brew4k@gmail.com>
2025-01-27 11:03:56 +0800

    Simplify testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,12 @@ import printer.PrettyPrint;
 import printer.Tester;
 
 class Test {
+    private final Pascal pascal_;
+
+    Test(Pascal pascal) {
+        pascal_ = pascal;
+    }
+
     static Result<String, Error> ok(String... output) {
         return Result.ok(String.join("\n", output));
     }
@@ -11,53 +17,50 @@ class Test {
         return String.join("\n", output);
     }
 
-    static void test01(Tester t, Pascal p) {
-        Result<String, Error> r;
+    private void test(String input, Result<String, Error> output) {
+        Assert.eq(pascal_.handle_cli_line(input), output);
+    }
 
-        r = p.handle_cli_line("hello");
-        Assert.eq(r, Result.err(Error.other("Invalid command. Try again.")));
+    static void test01(Pascal pascal) {
+        Test t = new Test(pascal);
 
-        r = p.handle_cli_line("todo read book");
-        Assert.eq(r, Result.ok(j("added: [T][ ] read book",
-                                 "Now you have 1 task in the list.")));
+        t.test("hello", Result.err(Error.other("Invalid command. Try again.")));
 
-        r = p.handle_cli_line("deadline return book /by June 6th");
-        Assert.eq(r, Result.ok(j("added: [D][ ] return book (by: June 6th)",
-                                 "Now you have 2 tasks in the list.")));
+        t.test("todo read book",
+               Result.ok(j("added: [T][ ] read book",
+                           "Now you have 1 task in the list.")));
 
-        r = p.handle_cli_line(
-            "event project meeting /from Aug 6th 2pm /to 4pm");
-        Assert.eq(
-            r, Result.ok(j(
+        t.test("deadline return book /by June 6th",
+               Result.ok(j("added: [D][ ] return book (by: June 6th)",
+                           "Now you have 2 tasks in the list.")));
+
+        t.test("event project meeting /from Aug 6th 2pm /to 4pm",
+               Result.ok(j(
                    "added: [E][ ] project meeting (from: Aug 6th 2pm to: 4pm)",
                    "Now you have 3 tasks in the list.")));
 
-        r = p.handle_cli_line("todo join sports club");
-        Assert.eq(r, Result.ok(j("added: [T][ ] join sports club",
-                                 "Now you have 4 tasks in the list.")));
+        t.test("todo join sports club",
+               Result.ok(j("added: [T][ ] join sports club",
+                           "Now you have 4 tasks in the list.")));
 
-        r = p.handle_cli_line("todo borrow book");
-        Assert.eq(r, Result.ok(j("added: [T][ ] borrow book",
-                                 "Now you have 5 tasks in the list.")));
+        t.test("todo borrow book",
+               Result.ok(j("added: [T][ ] borrow book",
+                           "Now you have 5 tasks in the list.")));
 
-        r = p.handle_cli_line("mark 1");
-        Assert.eq(r, Result.ok(j("Nice! I've marked this task as done:",
-                                 "[T][X] read book")));
+        t.test("mark 1", Result.ok(j("Nice! I've marked this task as done:",
+                                     "[T][X] read book")));
 
-        r = p.handle_cli_line("mark 4");
-        Assert.eq(r, Result.ok(j("Nice! I've marked this task as done:",
-                                 "[T][X] join sports club")));
+        t.test("mark 4", Result.ok(j("Nice! I've marked this task as done:",
+                                     "[T][X] join sports club")));
 
-        r = p.handle_cli_line("list");
-        Assert.eq(
-            r,
+        t.test(
+            "list",
             Result.ok(
                 j("1. [T][X] read book", "2. [D][ ] return book (by: June 6th)",
                   "3. [E][ ] project meeting (from: Aug 6th 2pm to: 4pm)",
                   "4. [T][X] join sports club", "5. [T][ ] borrow book")));
 
-        r = p.handle_cli_line("bye");
-        Assert.eq(r, Result.ok("Bye. Hope to see you again soon!"));
+        t.test("bye", Result.ok("Bye. Hope to see you again soon!"));
     }
 }
 
@@ -67,10 +70,8 @@ class Main {
     }
 
     static void run_tests() {
-        Tester t = new Tester();
-        Pascal p = new Pascal(System.in, t);
-
-        Test.test01(t, p);
+        Pascal p = new Pascal(System.in, new Tester());
+        Test.test01(p);
     }
 
     public static void main(String[] args) {
