136c9e02c07e3d84d4741dffaf31abf1be071058
nguyenvukhang <brew4k@gmail.com>
2025-01-27 10:19:58 +0800

    Update Result class

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -3,4 +3,17 @@ import java.util.Optional;
 public final class Result<T, E> {
     private Optional<T> value_;
     private Optional<E> err_;
+
+    private Result(Optional<T> value, Optional<E> error) {
+        value_ = value;
+        err_ = error;
+    }
+
+    public static <T, E> Result<T, E> of(T value) {
+        return new Result<>(Optional.of(value), Optional.empty());
+    }
+
+    public static <T, E> Result<T, E> err(E error) {
+        return new Result<>(Optional.empty(), Optional.of(error));
+    }
 }
