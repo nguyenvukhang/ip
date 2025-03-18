faac77eee9ab191c7f027f3a4d59a83bd04bd8bc
nguyenvukhang <brew4k@gmail.com>
2025-03-18 13:31:25 +0800

    Clean up Main.java

--- Makefile
+++ Makefile
@@ -2,14 +2,7 @@ MAKEFILE_PATH := $(abspath $(lastword $(MAKEFILE_LIST)))
 MAKEFILE_DIR  := $(dir $(MAKEFILE_PATH))
 GRADLE := $(MAKEFILE_DIR)gradlew
 
-current: gui
-
-quit:
-	$(GRADLE) run --args='quit'
-
-g: gui
-gui:
-	$(GRADLE) run --args='gui'
+current: run
 
 test:
 	$(GRADLE) test
@@ -19,21 +12,11 @@ jar:
 	rm -f ~/Downloads/pascal.jar
 	mv build/libs/pascal.jar ~/Downloads
 
-t:
-	DEBUG=1 $(GRADLE) run --args='test'
-
-i: interact
-interact: build
+run:
 	$(GRADLE) run --quiet --console=plain
 
 fmt:
 	$(GRADLE) spotlessApply
 
-c: checkstyleMain
-checkstyleMain:
-	$(GRADLE) checkstyleMain
-
 check:
 	$(GRADLE) check
-
-.PHONY: build gradle test
--- src/main/java/pascal/Main.java
+++ src/main/java/pascal/Main.java
@@ -1,64 +1,14 @@
 package pascal;
 
-// clang-format off
 import javafx.application.Application;
-import pascal.printer.Tester;
-import pascal.result.Error;
-import pascal.result.Result;
 import pascal.ui.App;
-// clang-format on
-
-class Test {
-    private final Pascal pascal;
-
-    Test(Pascal pascal) {
-        this.pascal = pascal;
-    }
-
-    static Result<String, Error> ok(String... output) {
-        return Result.ok(String.join("\n", output));
-    }
-
-    static String join(String... output) {
-        return String.join("\n", output);
-    }
-
-    private void test(String input, Result<String, Error> output) {
-        Assert.eq(pascal.handleUserInput(input), output);
-    }
-}
 
 /**
  * The entrypoint.
  */
 public class Main {
-    /** Quick and dirty check if we should run tests. */
-    static boolean isTest(String[] args) {
-        return args.length >= 1 && args[0].equals("test");
-    }
-
-    /** Quick and dirty check if we should just quit. */
-    static boolean isQuickQuit(String[] args) {
-        return args.length >= 1 && args[0].equals("quit");
-    }
-
-    /** Quick and dirty check if we should run the GUI. */
-    static boolean isGui(String[] args) {
-        return args.length >= 1 && args[0].equals("gui");
-    }
-
-    /** Test runner. */
-    static void runTests() {
-        Tester t = new Tester();
-    }
-
-    /** GUI runner. */
-    static void runGui(String[] args) {
-        Application.launch(App.class, args);
-    }
-
     /** The entrypoint. */
     public static void main(String[] args) {
-        runGui(args);
+        Application.launch(App.class, args);
     }
 }
